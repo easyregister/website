0f91fd2fe8592b963975d4bd8fe4ad9e10bf9f80
Gioele Pannetto <gioele@pannetto.com>
2021-09-22 14:14:53 +0200

    Added button linked to the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Easy Register</title>
       </Head>
-      <main className="flex h-screen items-center justify-center">
+      <main className="flex flex-col h-screen items-center justify-center gap-5">
         We are building the easy-to-use school register, because the future is today.
+
+        <a href="https://app.easyregister.it"><button className="bg-indigo-500 text-white rounded py-3 px-5 shadow">Go to the app</button></a>
       </main>
     </>
   )
